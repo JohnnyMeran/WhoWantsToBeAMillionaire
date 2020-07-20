e12dbfb6442773ed9a5f7bcbe98f1eefe7f2ef16
Catalin Tudorache <tumf87@gmail.com>
2020-07-20 22:09:46 +0300

    Added application objects.

--- src/main/java/ro/jademy/millionaire/Main.java
+++ src/main/java/ro/jademy/millionaire/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        // 1.  define objects
+            // define properties
+            // define methods
+
+        // 2. define interactions
+
+
+        // 1.
+        // Game
+        // Question
+        // Answer
+        // Level (???)
+        // Player (???)
+
+
+
     }
 }
